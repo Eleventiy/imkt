c7cf81691ef124a74054c74e9a72d3f4e13c723f
Leo <eleventiy96@gmail.com>
2017-12-13 23:13:10 +0200

    Finish all sections

--- gulpfile.js
+++ gulpfile.js
@@ -58,6 +58,8 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 				'app/libs/jquery/dist/jquery.min.js',
+				'app/libs/page-scroll-to-id/jquery.malihu.PageScroll2id.js',
+				'app/libs/slick-carousel/slick/slick.min.js',
 				'app/js/common.min.js' // Всегда в конце
 			])
 			.pipe(concat('scripts.min.js'))
